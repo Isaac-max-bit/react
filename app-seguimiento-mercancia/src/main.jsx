07f51e5241628b52226acacd6a6a06b1ab22d3eb
Isaac Garcia <garciaalejoisaac@gmail.com>
2025-02-26 21:13:33 -0500

    advance

--- app-seguimiento-mercancia/src/main.jsx
+++ app-seguimiento-mercancia/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import Encabezado from './Encabezado';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    {/* <App /> */}
+    {/* <h1>Hello Isaac, It's time to practice</h1> */}
+    <Encabezado />
   </StrictMode>
 );
