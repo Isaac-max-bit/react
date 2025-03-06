b0a2239e7e244c63fa614d5d555c26301ec8a85a
Desarrollo <samuelrojashurtado@gmail.com>
2025-03-06 08:11:21 -0500

    new✅

---
+++ src/Encabezado.jsx
@@ -0,0 +1,30 @@
+function Encabezado(){
+    return(
+        <header>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">Inicio</a>
+                    </li>
+
+                    <li>
+                        <a href="">Listar</a>
+                    </li>
+
+                    <li>
+                        <a href="">Crear</a>
+                    </li>
+
+                    <li>
+                        <a href="">Detalle</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Encabezado;
